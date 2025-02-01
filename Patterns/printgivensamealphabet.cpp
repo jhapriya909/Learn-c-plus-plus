2267dd35f70e90897260666a2c2008135f43b4fc
jhapriya909 <thepriyajha8840@gmail.com>
2025-02-01 15:00:47 +0530

    d:\C++\Learn-c-plus-plus\Patterns\printgivensamealphabet.cpp

---
+++ Patterns/printgivensamealphabet.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int n;
+    cout<<"Enter the number:";
+    cin>>n;
+    for(int i=1; i<=5; i++)
+    {
+        for(int j= 1; j<=4; j++)
+        {
+
+        }
+       cout<<(char)(i+65)<< " ";
+    }
+    cout<<endl;
+    
+}
