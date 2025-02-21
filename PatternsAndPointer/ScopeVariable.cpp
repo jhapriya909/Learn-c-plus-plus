68b6fb41009099dd3e61f47d9d7d9b8be9cdb4a6
jhapriya909 <thepriyajha8840@gmail.com>
2025-02-21 14:31:57 +0530

    d:\C++\Learn-c-plus-plus\PatternsAndPointer\ScopeVariable.cpp

---
+++ PatternsAndPointer/ScopeVariable.cpp
@@ -0,0 +1,24 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int i = 1; 
+    for(i=1; i<=5; i++)
+    {
+        cout<<i<<endl;
+    }
+    cout<<i<<endl;
+
+    // output 1to 6 print 
+
+   
+    // for(int i=1; i<=5; i++)
+    // {
+    //     cout<<i<<endl;
+    // }
+    // cout<<i<<endl;
+
+    // output will be error 
+
+
+}
