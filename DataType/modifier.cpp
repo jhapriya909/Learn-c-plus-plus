0b3038f6277a05791510c90e2f1f67bacb4d603b
jhapriya909 <thepriyajha8840@gmail.com>
2025-04-09 21:32:54 +0530

    d:\C++\Learn-c-plus-plus\DataType\modifier.cpp

---
+++ DataType/modifier.cpp
@@ -0,0 +1,9 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+ unsigned smallteapack = -100;
+ long long largeteastorage = 100000000000;
+ short teasample = 29;
+    cout<< largeteastorage <<endl;
+}
