9e755bb5286856e75274e88158395f3c277b6846
jhapriya909 <thepriyajha8840@gmail.com>
2025-04-09 21:32:16 +0530

    d:\C++\Learn-c-plus-plus\DataType\data.cpp

---
+++ DataType/data.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int tealeaves = 295;
+    float waterTempertaure = 98.2569999999;
+    double teaprice = 9.987654;
+    char teagrade = 'A';
+    bool isteaready = false;
+
+    cout<< waterTempertaure <<endl;
+    return 0;
+}
