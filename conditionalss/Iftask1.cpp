3269187d91cb71617326857972a59106d0e883ae
jhapriya909 <thepriyajha8840@gmail.com>
2025-04-09 21:23:49 +0530

    d:\C++\Learn-c-plus-plus\conditionalss\Iftask1.cpp

---
+++ conditionalss/Iftask1.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+#include<string>
+using namespace std;
+int main()
+{
+  string ordertea;
+  cout<<"enter your tea order"<<endl;
+  cin>>ordertea;
+
+  if(ordertea=="Green tea")
+  {
+    cout<<"you have order Green tea"<<endl;
+  }
+
+
+   }
