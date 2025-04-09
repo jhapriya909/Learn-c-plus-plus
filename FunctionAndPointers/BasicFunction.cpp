55f788362194021571be4f0509fe94390a1b3773
jhapriya909 <thepriyajha8840@gmail.com>
2025-04-09 21:25:59 +0530

    d:\C++\Learn-c-plus-plus\FunctionAndPointers\BasicFunction.cpp

---
+++ FunctionAndPointers/BasicFunction.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+using namespace std;
+  //parenthesis()    //this is a function
+
+    void usa()
+    {
+        cout<<"You are in USA"<<endl;
+    }
+    
+    void india()
+    {
+        cout<<"You are in India"<<endl;
+    }
+
+    int main()
+    {
+        cout<<"You are in main"<<endl;
+        india();
+        usa();
+    }
