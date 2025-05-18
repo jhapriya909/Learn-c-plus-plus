2321365e31382245706876116309be6005378242
jhapriya909 <thepriyajha8840@gmail.com>
2025-05-18 20:59:01 +0530

    d:\C++\Learn-c-plus-plus\LoopPractice\ForLoop.cpp

---
+++ LoopPractice/ForLoop.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int teacups = 5;
+    cout<<"Enter a tea cups :";
+    cin>>teacups;
+
+
+    for(int i = 1; i<=teacups; i++){
+    cout<<"Brewing cups "<<  i <<" of tea......" <<endl;
+
+
+    }
+
+
+}
