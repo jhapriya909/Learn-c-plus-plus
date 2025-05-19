03cf8768af516bd7962cf7a3d8f9a26e8bd66050
jhapriya909 <thepriyajha8840@gmail.com>
2025-05-19 12:05:59 +0530

    d:\C++\Learn-c-plus-plus\LoopPractice\Function.cpp\PassByReference.cpp

---
+++ LoopPractice/Function.cpp/PassByReference.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+  void pourchai(int & cups){
+    cups = cups+1;
+    cout<<"Poured" <<cups  <<"cups of chai!"<<endl;
+
+
+  }
+
+  int main(){
+    int chaicups = 2;
+    pourchai (chaicups);
+     cout << "Total chai cups outside function: "<<chaicups << endl;
+  return 0;
+}
