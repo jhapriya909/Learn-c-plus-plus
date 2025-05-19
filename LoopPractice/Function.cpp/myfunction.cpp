ba9299abd8a9ceb23778ee11fffbe3d7df8b301f
jhapriya909 <thepriyajha8840@gmail.com>
2025-05-19 12:05:33 +0530

    d:\C++\Learn-c-plus-plus\LoopPractice\Function.cpp\myfunction.cpp

---
+++ LoopPractice/Function.cpp/myfunction.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+// Function to check tea temperature
+
+int checkTeaTemperature(int Temperature){
+    return Temperature;
+}
+
+int main(){
+    int Temp = checkTeaTemperature(85);   // Function call
+    cout<<"The Tea Temperature"<<Temp <<"°C"<<endl;
+    return 0;
+}
