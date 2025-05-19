2457245180150fe30387cd6665a2fb34beb1e784
jhapriya909 <thepriyajha8840@gmail.com>
2025-05-19 12:03:10 +0530

    d:\C++\Learn-c-plus-plus\LoopPractice\Function.cpp\DefiningFuction.cpp

---
+++ LoopPractice/Function.cpp/DefiningFuction.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+using namespace std;
+void makechai(){
+    cout<<"Boiling water,adding the tea leaves,and serving a chai!"<<endl;
+
+}
+int main(){
+    makechai();
+    return 0;
+}
