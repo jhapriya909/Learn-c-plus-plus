95a23e9f5a9bde79913cc476e020894fd59a0d59
jhapriya909 <thepriyajha8840@gmail.com>
2025-05-22 09:04:55 +0530

    d:\C++\Learn-c-plus-plus\Function\DefiningFunction.cpp

---
+++ Function/DefiningFunction.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+// Function definition with body
+void makeChai() {
+  cout << "Boiling water, adding tea leaves, and serving chai!" << endl;
+}
+
+int main() {
+  makeChai();  // Calling the function
+  return 0;
+}
