79788cf03b1560c6f939fcc658858b29db5f218b
jhapriya909 <thepriyajha8840@gmail.com>
2025-05-22 09:03:45 +0530

    d:\C++\Learn-c-plus-plus\Function\CallingFunction.cpp

---
+++ Function/CallingFunction.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+// Function to brew tea
+void brewChai() {
+  cout << "Chai is being brewed!" << endl;
+}
+
+int main() {
+  brewChai();  // Function call
+  return 0;
+}
