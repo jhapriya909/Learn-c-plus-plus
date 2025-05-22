2b67571065b50d5a2e44b0c931b43ddb823064e9
jhapriya909 <thepriyajha8840@gmail.com>
2025-05-22 22:13:11 +0530

    d:\C++\Learn-c-plus-plus\ArrayDynamicMemory.cpp\one.cpp

---
+++ ArrayDynamicMemory.cpp/one.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+int main(){
+
+    int chaiTempertaure[5] = {85,89,90,100, 120};
+    cout<<"  Chai Temperature";
+
+    for(int i = 0; i<5; i++){
+        cout<<  chaiTempertaure[i] <<"degree  \n";
+    }
+    return 0;
+
+}
