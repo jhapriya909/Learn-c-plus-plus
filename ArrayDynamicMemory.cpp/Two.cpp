e1ef6286ad7d79bd9e9d9ec8524383a9a280579f
jhapriya909 <thepriyajha8840@gmail.com>
2025-05-22 22:13:45 +0530

    d:\C++\Learn-c-plus-plus\ArrayDynamicMemory.cpp\Two.cpp

---
+++ ArrayDynamicMemory.cpp/Two.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int chaiServed[6] = {67,89,90,98,67,65};
+
+    cout<<" chai served on Day 1:"<<chaiServed[0] <<endl;
+    cout<<" chai served on Day 2:"<<chaiServed[1] <<endl;
+    cout<<" chai served on Day 3:"<<chaiServed[2] <<endl;
+    cout<<" chai served on Day 4:"<<chaiServed[3] <<endl;
+    cout<<" chai served on Day 5:"<<chaiServed[4] <<endl;
+    cout<<" chai served on Day 6:"<<chaiServed[5] <<endl;
+    return 0;
+}
