36de77c6f9ce6eae9b0816bbe40466d476baf4fd
jhapriya909 <thepriyajha8840@gmail.com>
2025-05-23 21:08:10 +0530

    d:\C++\Learn-c-plus-plus\DSABasicQuestion\IF-elseTask2.cpp

---
+++ DSABasicQuestion/IF-elseTask2.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+
+using namespace std;
+int main()
+{
+    int hour;
+    cout<<"enter your current   hour (0-23):"<<endl;
+    cin>>hour;
+
+    if(hour>= 8 && hour <= 18)
+    {
+        cout<<"Tea shop is open"<<endl;
+
+    }
+    else{
+        cout<<"Tea shop is closed"<<endl;
+    }
+    return 0;
+   
+   
+}
