b9360941c0cf1fc51d641ccca58ec2dbf0609b10
jhapriya909 <thepriyajha8840@gmail.com>
2025-05-25 20:13:19 +0530

    d:\C++\Learn-c-plus-plus\QuestionPractice50\ArmstrongNumbers.cpp

---
+++ QuestionPractice50/ArmstrongNumbers.cpp
@@ -0,0 +1,23 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int n, sum = 0;
+    cout<<"Enter any number:";
+    cin>>n;   // n = 153
+
+    int temp = n;    // temp = 153
+    while(n!= 0){    // 153! = 0true
+        int rem = n%10;   // rem = 3
+        sum = sum + rem*rem*rem;   // sum = 0+3*3*3 = 27
+        n = n/10;   // 153/10 = 15
+
+    }
+    if(temp == sum )
+    cout<<" A armstrong Numbers"<<sum;
+
+    else{
+        cout<<"not a armstrong Numbers:"<<sum;
+        return 0;
+    }
+
+}
