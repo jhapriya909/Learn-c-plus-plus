8b98b0af4c1785ed673f5a5be03d2cbaa38dc676
jhapriya909 <thepriyajha8840@gmail.com>
2025-05-27 09:56:56 +0530

    d:\C++\Learn-c-plus-plus\QuestionPractice50\FibonacciSeries.cpp

--- QuestionPractice50/FibonacciSeries.cpp
+++ QuestionPractice50/FibonacciSeries.cpp
@@ -1,18 +1,18 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
-int main(){
-    int num, first = 0,second = 1 ,next;
-    cout<<"Enter the number  to be printed  Fibonacci Series"<<endl;
-    cin>>num;
-    cout<<"Fibonacci Series:";
+int main()
+{
+    int num, first = 0, second = 1, next;
+    cout << "Enter the number  to be printed  Fibonacci Series" << endl;
+    cin >> num;
+    cout << "Fibonacci Series:";
 
-    for( int i = 0; i<num; i++){
+    for (int i = 0; i < num; i++)
+    {
 
-        cout<<first <<endl; // 0
+        cout << first << endl; // 0
         next = first + second; // 0+1 = 1
-        first = second; // update = 1
-        second = next; // 1
-
+        first = second;        // update = 1
+        second = next;         // 1
     }
-
 }
