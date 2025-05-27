b72fe319c82565e6dd9e658c24c57ac46bf8133e
jhapriya909 <thepriyajha8840@gmail.com>
2025-05-27 09:55:46 +0530

    d:\C++\Learn-c-plus-plus\QuestionPractice50\Primenumber.cpp

--- QuestionPractice50/Primenumber.cpp
+++ QuestionPractice50/Primenumber.cpp
@@ -1,20 +1,21 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
 int main()
 {
     int n;
-    cout<<"Enter n:";
-    cin>>n;
-    bool flag=true;  // number is prime
-    for(int i=2; i<=n/2; i++)
+    cout << "Enter n:";
+    cin >> n;
+    bool flag = true; // number is prime
+    for (int i = 2; i <= n / 2; i++)
     {
-        if(n%i==0)
+        if (n % i == 0)
         {
-             flag=false; // false means not prime
-        break;
+            flag = false; // false means not prime
+            break;
         }
-       
     }
-    if(flag==true) cout<<"prime number";
-    else  cout<<"Not prime number";
+    if (flag == true)
+        cout << "prime number";
+    else
+        cout << "Not prime number";
 }
